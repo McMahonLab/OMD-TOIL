0a400bd5504a2b071804a5f30ad157959ec29a17
Josh Hamilton <joshamilton@gmail.com>
2016-01-29 12:44:11 -0600

    Update mapping script to use BBMap

--- scripts/10_mapping/10b_uncompetitive/01uncompReadMapping.pl
+++ scripts/10_mapping/10b_uncompetitive/01uncompReadMapping.pl
@@ -40,24 +40,10 @@ my @genomeList = glob($genomeFolder.'/*.fna');
 my @sampleList = glob($mtFolder.'/*');
 
 ################################################################################
-### Step 1: Index Genomes
+### Step 2: Simultaneously Index and Map Metatranscriptomes to Reference Genomes
 ################################################################################
 
 my $int = 1;
-foreach my $genomePath (@genomeList) {
-  print "Indexing genome ".$int." of ".@genomeList."\n";
-  if ($genomePath =~ /(.+).fna/) {
-    my $genome = $1;
-    system("bwa index -p $genome -a is $genomePath");
-    $int++;
-    }
-}
-
-################################################################################
-### Step 2: Map Metatranscriptomes to Reference Genomes
-################################################################################
-
-$int = 1;
 my $total = @genomeList*@sampleList;
 
 foreach my $samplePath (@sampleList) {
@@ -68,29 +54,8 @@ foreach my $samplePath (@sampleList) {
 	my $sample = $1;
 	print "Mapping sample ".$sample." against genome ".$genome." (".$int." of ".$total."). \n";
 	$int ++;
-	system("bwa mem -t ".$numProcs." ".$genomeFolder."/".$genome." ".$samplePath."/rRNA_removal/".$sample."_non_rRNA.fastq > ".$mapFolder."/".$sample."-".$genome.".sam");
+	system("bbmap.sh t=".$numProcs." in=".$samplePath."/rRNA_removal/".$sample."_non_rRNA.fastq outm=".$mapFolder."/".$sample."-".$genome.".sam ref=".$genomeFolder."/".$genome.".fna nodisk sam=1.3");
       }
     }
   }
 }
-
-################################################################################
-### Step 3: Manipulate Output SAM files. Convert to BAM, sort and index. Delete
-### unsorted SAM and BAM files to save space.
-################################################################################
-
-#my @samList = glob($mapFolder.'/*.sam');
-#$int = 1;
-
-#foreach my $samPath (@samList) {
-#  print "Processing SAM file ".$int." of ".@samList."\n";
-#  if ($samPath =~ /.+\/(.+).sam/) {
-#    my $sam = $1;
-#    system("samtools view -b -S -o ".$mapFolder."/".$sam.".bam ".$mapFolder."/".$sam.".sam");
-#    system("samtools sort -o ".$mapFolder."/".$sam.".sorted.bam -O bam -T ".$mapFolder."/temp/".$sam." ".$mapFolder."/".$sam.".bam");
-#    system("samtools index ".$mapFolder."/".$sam.".sorted.bam");
-#    system("rm ".$mapFolder."/".$sam.".bam");
-#    system("rm ".$mapFolder."/".$sam.".sam");
-#    $int ++;
-#  }
-#}
