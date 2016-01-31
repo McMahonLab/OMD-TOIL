c54c8d26a5e4d796a5368548a8954a10d75afbe2
Josh Hamilton <joshamilton@gmail.com>
2016-01-31 17:31:48 -0600

    Convert readCounts scripts to parallel operation

--- scripts/10_mapping/10b_uncompetitive/02uncompReadCountsCDS.pl
+++ scripts/10_mapping/10b_uncompetitive/02uncompReadCountsCDS.pl
@@ -15,6 +15,7 @@ use warnings;
 
 # Import packages
 use File::Path qw(make_path);
+use Parallel::ForkManager;
 
 ################################################################################
 ### User-defined files and folder structure
@@ -54,15 +55,31 @@ while (my $dir = readdir(DIR)) {
 closedir(DIR);
 
 ################################################################################
+### Generate a list of commands to run
+################################################################################
+
+my @commandArray;
+
+foreach my $samplePath (@sampleList) {
+  foreach my $genomePath (@genomeList) {
+    push @commandArray, '/usr/local/bin/htseq-count -f sam -r pos -s no -a 0 -t CDS -i locus_tag -m intersection-strict -o '. $countFolder.'/'.$mt.'-'.$genome.'.CDS.sam '.$mapFolder.'/'.$mt.'-'.$genome.'.sam '.$gffFolder.'/'.$genome.'.gff >  '.$countFolder.'/'.$mt.'-'.$genome.'.CDS.out';
+  }
+}
+################################################################################
 ### Loop to obtain count data
 ################################################################################
 
-# Loop over each genome and MT
-foreach my $genome (@genomeList) {
-    print "Processing genome ".$genome."\n";
-    foreach my $mt (@mtList) {
-      print "Processing metranscriptome ".$mt."\n";
-      # Call HTseq-count
-      system('/usr/local/bin/htseq-count -f sam -r pos -s no -a 0 -t CDS -i locus_tag -m intersection-strict -o '. $countFolder.'/'.$mt.'-'.$genome.'.CDS.sam '.$mapFolder.'/'.$mt.'-'.$genome.'.sam '.$gffFolder.'/'.$genome.'.gff >  '.$countFolder.'/'.$mt.'-'.$genome.'.CDS.out');
-      }
-  }
+my $numChildren = 25;
+my $pm = new Parallel::ForkManager($numChildren);
+
+foreach my $command (@commandArray) {
+  # Forks and returns the pid for the child:
+  my $pid = $pm->start and next;
+
+  system($command);
+  
+  $pm->finish; # Terminates the child process
+}
+
+$pm->wait_all_children;
+print "Counting complete!\n";
